79ed5fd815c20f6c199e2fc6c9fa04d6e78584a4
Egor14 <egorgrekhnev@mail.ru>
2020-04-03 11:33:08 +0300

    unique user_id

--- gold_coin/coin_info/models.py
+++ gold_coin/coin_info/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TokenInfo(models.Model):
-    user_id = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=50, unique=True)
     ducatus_address = models.CharField(max_length=50, null=True, default=None)
     ducatusx_address = models.CharField(max_length=50, null=True, default=None)
     token_type = models.CharField(max_length=50)
